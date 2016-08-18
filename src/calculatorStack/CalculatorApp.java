57ae1adcb0cc0a531539e0c30f00277b5a7fa8c4
Grace Morse <msgracemorse@gmail.com>
2016-08-18 16:53:57 -0400

    Calculator Stack Lab

---
+++ src/calculatorStack/CalculatorApp.java
@@ -0,0 +1,76 @@
+package calculatorStack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class CalculatorApp {
+
+	public static void main(String[] args) {
+		
+		Stack <Integer> s = new Stack <Integer>();
+		Scanner scan1 = new Scanner(System.in);
+		
+		System.out.println("What do you want to calculate?");
+		
+		String input = scan1.nextLine();
+		scan1.nextLine();
+		int singDing = 0;
+			
+		for (int i=0; i<input.length(); i++){
+			char singleDigit = input.charAt(i);
+			
+			if (singleDigit == '0' ||singleDigit == '1'|| singleDigit == '2'||singleDigit == '3' ||
+				singleDigit == '4' ||singleDigit == '5'|| singleDigit == '6'||singleDigit == '7' ||
+				singleDigit == '8' ||singleDigit == '9'){
+	
+			singDing = Character.getNumericValue(singleDigit);
+		
+			s.push(singDing);
+			
+		}   else if (singleDigit == '+'){
+			
+			int x = s.pop();
+			int y = s.pop();
+						
+			int result = y + x;
+				
+			System.out.println(y + "+" + x + "=" + result);
+			
+			s.push(result);
+				
+		}	else if (singleDigit == '-'){
+			
+			int x = s.pop();
+			int y = s.pop();
+						
+			int result = y - x;
+				
+			System.out.println(y + "-" + x + "=" + result);
+			
+			s.push(result);
+			
+		}	else if (singleDigit == '*'){
+			
+			int x = s.pop();
+			int y = s.pop();
+						
+			int result = y * x;
+				
+			System.out.println(y + "*" + x + "=" + result);
+					
+			s.push(result);
+			
+		}   else if (singleDigit == '/'){
+			
+			int x = s.pop();
+			int y = s.pop();
+						
+			int result = y / x;
+				
+			System.out.println(y + "/" + x + "=" + result);
+			
+			s.push(result);
+		}
+}	
+}	
+}
